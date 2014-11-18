1993a237ca6c7a5db52d7c1c5a672625a051be31
Philipp <philipp.knuesel@database-consulting.ch>
2014-11-18 22:29:00 +0100

    first version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: Philipp
+## Date:   2014-11-18
+## Assignment 2: Inverse Matrix
 
-## Write a short comment describing this function
+# set up "matrix" with cacheable inverted value 
+# (list of functions:
+#  set matrix
+#  get matrix
+#  set (cache) inverse matrix
+#  get inverse matrix from cache)
+# 
+# note: matrix with det(matrix) = 0 can't be inverted
+# see assignement
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) invm <<- solve
+  getinv <- function() invm
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv
+  )
 }
 
 
-## Write a short comment describing this function
+# returns cached value (inverted matrix)
+# calculates inverted matrix if not cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  minv <- x$getinv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data)
+  x$setinv(minv)
+  minv
 }
+
